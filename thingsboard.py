1670936aa743f787407b29342f9cb858be0e2690
dangiathinh <danggiathinhgocong@gmail.com>
2023-03-06 09:40:59 +0700

    first push

---
+++ thingsboard.py
@@ -0,0 +1,102 @@
+import serial.tools.list_ports
+import paho.mqtt.client as mqttclient
+import time
+import json
+
+BROKER_ADDRESS = "demo.thingsboard.io"
+PORT = 1883
+THINGS_BOARD_ACCESS_TOKEN = "V7mNA0GKKLF6Ju2OlSNQ"
+
+
+
+def subscribed(client, userdata, mid, granted_qos):
+    print("Subscribed...")
+
+
+def recv_message(client, userdata, message):
+    print("Received: ", message.payload.decode("utf-8"))
+    temp_data = {'value': True}
+    try:
+        jsonobj = json.loads(message.payload)
+        if jsonobj['method'] == "setValue":
+            temp_data['value'] = jsonobj['params']
+            client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
+    except:
+        pass
+
+
+def connected(client, usedata, flags, rc):
+    if rc == 0:
+        print("Thingsboard connected successfully!!")
+        client.subscribe("v1/devices/me/rpc/request/+")
+    else:
+        print("Connection is failed")
+
+
+client = mqttclient.Client("Gateway_Thingsboard")
+client.username_pw_set(THINGS_BOARD_ACCESS_TOKEN)
+
+client.on_connect = connected
+client.connect(BROKER_ADDRESS, 1883)
+client.loop_start()
+
+client.on_subscribe = subscribed
+client.on_message = recv_message
+
+
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        if "USB Serial Device" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = (splitPort[0])
+    return commPort
+
+
+isMicrobitConnected = False
+if getPort() != "None":
+    ser = serial.Serial( port=getPort(), baudrate=115200)
+    isMicrobitConnected = True
+
+mess = ""
+entry_dict = {
+    "TEMPERATURE": "",
+    "HUMIDITY": "",
+}
+
+//!TEMPERATURE:26#
+def processData(data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    entry_dict["TEMPERATURE"] = splitData[0]
+    entry_dict["value"] = splitData[1]
+    client.publish("v1/devices/me/telemetry", json.dumps(entry_dict))
+
+
+mess = ""
+def readSerial():
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
+
+
+while True:
+    if isMicrobitConnected:
+        readSerial()
+
+    time.sleep(1)
